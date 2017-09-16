0fe03c59ea17475b9f2219fe87938e2711bbba5e
fcostaa <felipe.h.j.costa@gmail.com>
2017-09-16 17:20:13 -0300

    Remove JaCoCo command on Travis for now

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -2,8 +2,8 @@
 i
 if [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew clean build lib:jacocoTestReport lib:bintrayUpload -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_PASSWORD}" -PdryRun=false
+  ./gradlew clean build lib:bintrayUpload -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_PASSWORD}" -PdryRun=false
 else
     echo -e 'Normal Build => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew build lib:jacocoTestReport
+  ./gradlew clean build
 fi

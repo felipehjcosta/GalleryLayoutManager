75a6127c64b2c5f0fa6be8cd70449d140f498b51
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-02-01 13:35:02 -0300

    Fix typo

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-i
+
 if [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   ./gradlew clean build lib:bintrayUpload -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_PASSWORD}" -PdryRun=false
